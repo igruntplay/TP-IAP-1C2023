38c01fbbe2207f1212179b63a50f15ef57bc3ffb
daferuba <118111811+daferuba@users.noreply.github.com>
2023-05-17 17:43:14 -0700

    Refactor+AlgunComment
    Refactorice red para que siempre se llame red, l paso a ser likes, p es publicacion y deje como abstractas las funciones abstractas de conjuntos. Me gustaria que alguien mas lo chequeara.
    falta comentar, ej4, ej6, ej7 y ej8 y ni hablar de que falta posta un monton de chequear COMO estan escritos los comentarios.
    curryficacion ejercicio 4
    No entiendo si en el ej 5 tambien tiene que estar el usuario que se LLAME roberto carlos.

--- Solucion.hs
+++ Solucion.hs
@@ -32,8 +32,10 @@ likesDePublicacion (_, _, us) = us
 
 {- Ejercicio 1:
     La función nombreDeUsuarios recibe un tipo de dato RedSocial y devuelve una lista de strings con los nombres de los usuarios.
-    En la implementación desestructurizo la tupla para obtener una lista de usuarios de la red social y llamo a proyectarNombres para obtener una lista con los nombres de los usuarios.
-    La función usuarios se utiliza para separar la información de los usuarios de la estructura total de la red social. dejando que la función nombresDeUsuario trabaje unicamente con la lista de los usuarios.
+    En la implementación desestructurizamos la tupla para obtener una lista de usuarios de la red social y llamo a proyectarNombres
+    para obtener una lista con los nombres de los usuarios.
+    La función usuarios se utiliza para separar la información de los usuarios de la estructura total de la red social
+    dejando que la función nombresDeUsuario trabaje unicamente con la lista de los usuarios.
 -}
 nombresDeUsuarios :: RedSocial -> [String]
 nombresDeUsuarios red = proyectarNombres (usuarios red)
@@ -42,7 +44,6 @@ nombresDeUsuarios red = proyectarNombres (usuarios red)
     La función proyectarNombres toma una lista de usuarios y devuelve una lista de strings con los nombres de los usuarios.
     Uso recursión parar recorrer la lista y obtener los nombres agregandolos a una lista vacía.
 -}
-
 proyectarNombres :: [Usuario] -> [String]
 proyectarNombres [] = []
 proyectarNombres (u:us) = nombreDeUsuario u : proyectarNombres us
@@ -51,11 +52,11 @@ proyectarNombres (u:us) = nombreDeUsuario u : proyectarNombres us
 {- Ejercicio 2:
     amigosDe recibe un tipo de dato RedSocial, un tipo Usuario, y nos devuelve una lista de Usuarios.
     La funcion la modularizamos, primero obteniendo del tipo RedSocial su lista de Relacion, en la que se uso la funcion relaciones.
-    Una vez obtenida se llama a la funcion estanRelacionados que recibe una lista de Relacion y un Usuario, donde la lista que nos devuelve son todos usuarios que comparten un tipo Relacion en el que pertenecen ambos.
+    Una vez obtenida se llama a la funcion estanRelacionados que recibe una lista de Relacion y un Usuario,
+    donde la lista que nos devuelve son todos usuarios que comparten un tipo Relacion en el que pertenecen ambos.
 -}
-
 amigosDe :: RedSocial -> Usuario -> [Usuario]
-amigosDe x y = estanRelacionados (relaciones x) y
+amigosDe red u = estanRelacionados (relaciones red) u
 
 {- 
     Esta funcion plantea un recursion sobre una listaa de tipo Relacion=(Usuario,Usuario). 
@@ -63,39 +64,43 @@ amigosDe x y = estanRelacionados (relaciones x) y
     Si lo es, agrega el Usuario a una lista que se genera haciendo el llamado recurisvo. Si no lo es simplemente lo omite y sigue recorriendo la lista.
     Una vez que llego al final se encuentra con la lista vacia que es nuestro caso base y la recursion termina y nos devuelve la lista de Usuarios que se relacionan. 
 -}
-
 estanRelacionados :: [Relacion] -> Usuario -> [Usuario]
 estanRelacionados [] u = []
 estanRelacionados ((u1,u2):rs) u | u1 == u = u2 : estanRelacionados rs u
-                       | u2 == u = u1 : estanRelacionados rs u
-                       | otherwise = estanRelacionados rs u
+                                 | u2 == u = u1 : estanRelacionados rs u
+                                 | otherwise = estanRelacionados rs u
 
 {- Ejercicio 3
     La funcion cantidadDeAmigos recibe un tipo RedSocial y un tipo Usuario, y nos devuelve un Integer.
     Hicimos uso de la funcion amigosDe para poder obtener una lista de Usuarios donde los usuarios que aparecieran sean todos los amigos del Usuario.
-    Luego llamamos a la funcion longitud que nos devuelve la cantidad de elementos que hay en la lista de usuarios que le pasamos, que corresponde a la cantidad de amigos del usuario. 
+    Luego llamamos a la funcion longitud que nos devuelve la cantidad de elementos que hay en la lista de usuarios que le pasamos,
+    que corresponde a la cantidad de amigos del usuario. 
 -}
 cantidadDeAmigos :: RedSocial -> Usuario -> Integer
 cantidadDeAmigos red u = longitud (amigosDe red u) 
 
 {-
-    longitud suma 1 por cada elemento de la lista. Una vez que la lista esta vacia suma 0 y finaliza.
+    Longitud suma 1 por cada elemento de la lista. Una vez que la lista esta vacia suma 0 y finaliza.
 -}
 longitud :: [t] -> Integer
 longitud [] = 0
 longitud (x:xs) = 1 + longitud xs
 
--- describir qué hace la función: ..... Ejercicio 4
+{- Ejercicio 4 
+    Falta explicar este ejercicio
+-}
 usuarioConMasAmigos :: RedSocial -> Usuario
-usuarioConMasAmigos r = usuarioPopular r (usuarios r)
+usuarioConMasAmigos red = usuarioPopular red (usuarios red)
 
+{-
+    Falta explicar esta auxiliar y explicar que tiene curryficacion en amigos = cantidadDeAmigos red
+-}
 usuarioPopular :: RedSocial -> [Usuario] -> Usuario
-usuarioPopular r [] = undefined
-usuarioPopular r [x] = x
-usuarioPopular r (x:xs:xss) | cAm r x >= cAm r xs = usuarioPopular r (x:xss)
-                            | otherwise = usuarioPopular r (xs:xss)
-                            where cAm = cantidadDeAmigos
-
+usuarioPopular red [] = undefined
+usuarioPopular red [u] = u
+usuarioPopular red (u1:u2:us) | amigos u1 >= amigos u2 = usuarioPopular red (u1:us)
+                              | otherwise = usuarioPopular red (u2:us)
+                              where amigos = cantidadDeAmigos red
 
 {- Ejercicio 5 
     estaRobertoCarlos recibe un tipo RedSocial y devuelve un booleano
@@ -113,33 +118,33 @@ estaRobertoCarlos ((u:us),(r:rs),_) | cantidadDeAmigos ([],(r:rs),[]) u > 10 = T
 -}
 publicacionesDe :: RedSocial -> Usuario -> [Publicacion]
 publicacionesDe (_,_,[]) u = []
-publicacionesDe (_,_,(x:xs)) u | usuarioDePublicacion x == u = x : publicacionesDe ([],[],xs) u
-                               | otherwise = publicacionesDe ([],[],xs) u
+publicacionesDe (_,_,(p:ps)) u | usuarioDePublicacion p == u = p : publicacionesDe ([],[],ps) u
+                               | otherwise = publicacionesDe ([],[],ps) u
 
 {- Ejercicio 7
 
 -}
 publicacionesQueLeGustanA :: RedSocial -> Usuario -> [Publicacion]
 publicacionesQueLeGustanA (_,_,[]) u = []
-publicacionesQueLeGustanA (_,_,(x:xs)) u | pertenece (likesDePublicacion x) u = x : publicacionesQueLeGustanA ([],[],xs) u
-                                         | otherwise = publicacionesQueLeGustanA ([],[],xs) u
+publicacionesQueLeGustanA (_,_,(p:ps)) u | pertenece u (likesDePublicacion p) = p : publicacionesQueLeGustanA ([],[],ps) u
+                                         | otherwise = publicacionesQueLeGustanA ([],[],ps) u
 
-pertenece :: Eq a => [a] -> a -> Bool
-pertenece [] y = False
-pertenece (x:xs) y = x == y || pertenece xs y
+pertenece :: Eq a => a -> [a] -> Bool
+pertenece y [] = False
+pertenece y (x:xs) = x == y || pertenece y xs
 
 {- Ejercicio 8
 
 -}
 lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool 
-lesGustanLasMismasPublicaciones r u1 u2 = sonLaMismaLista (publicacionesQueLeGustanA r u1) (publicacionesQueLeGustanA r u2) 
-
+lesGustanLasMismasPublicaciones red u1 u2 = sonLaMismaLista (likes u1) (likes u2)
+                                          where likes = publicacionesQueLeGustanA red
 
 sonLaMismaLista :: Eq a => [a] ->[a]->Bool
 sonLaMismaLista [] [] = True
 sonLaMismaLista [] _ = False
 sonLaMismaLista _ [] = False
-sonLaMismaLista (x:xs) (y:ys) = x==y && sonLaMismaLista xs ys
+sonLaMismaLista (x:xs) (y:ys) = x == y && sonLaMismaLista xs ys
 
 {- Ejercicio 9
     tieneUnSeguidorFiel recibe un tipo RedSocial, un Usuario y devuelve un booleano.
@@ -147,42 +152,42 @@ sonLaMismaLista (x:xs) (y:ys) = x==y && sonLaMismaLista xs ys
     Esas dos listas se la pasamos a la funcion aAlguienLeGustanTodas
 -}
 tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
-tieneUnSeguidorFiel r u = aAlguienLeGustanTodas (usuarios r) (publicacionesDe r u)
+tieneUnSeguidorFiel red u = aAlguienLeGustanTodas (usuarios red) (publicacionesDe red u)
 
 {-
     aAlguienLeGustanTodas nos devuelve un booleano chequeando si es verdadera la funcion leGustanTodasLasPublicaciones, 
     que en el caso de False llama a la recursion para fijarse si al siguiente de la lista de usuarios le gustan todas las publicaciones
 -}
-
 aAlguienLeGustanTodas :: [Usuario] -> [Publicacion] -> Bool
 aAlguienLeGustanTodas (u:us) ps = leGustanTodasLasPublicaciones u ps || aAlguienLeGustanTodas us ps
 
 {-
     leGustanTodasLasPublicaciones recibe un Usuario y una lista de tipo Publicacion y nos devuelve un booleano
     el caso base sucede cuando la lista de publicaciones, que sucede cuando la recursion recorrio toda la lista sin caer en el False
-    cuando la lista no es vacia ingresa al elemento correspondiente a los "Likes" de la publicacion (que es una lista de Usuarios) y se fija si el usuario que le dimos pertenece a esa lista y por la recursion, que suceda para todas las publicaciones
+    cuando la lista no es vacia ingresa al elemento correspondiente a los "Likes" de la publicacion (que es una lista de Usuarios)
+    y se fija si el usuario que le dimos pertenece a esa lista y por la recursion, que suceda para todas las publicaciones
     en el caso de que sea False la funcion anterior le entrega el usuario siguiente para repetir el proceso.
-
 -}
-
 leGustanTodasLasPublicaciones :: Usuario -> [Publicacion] -> Bool
 leGustanTodasLasPublicaciones u [] = True
-leGustanTodasLasPublicaciones u ((_,_,l):ps) = pertenece l u && leGustanTodasLasPublicaciones u ps
-
- 
-    
+leGustanTodasLasPublicaciones u ((_,_,likes):ps) = pertenece u likes && leGustanTodasLasPublicaciones u ps
 
 {- Ejercicio 10
-    La funcion existeSecuenciaDeAmigos toma una RedSocial y dos usuarios. Devuelve True si existe una secuencia de relaciones entre usuarios que conecte al usuario 1 con el usuario 2 y devuelve False si no existe una secuencia de relaciones que los conecten o si estan directamente relacionados. 
-    Este problema era bastante complicado, por lo que encontramos un problema equivalente que nos solucionara la misma situacion. Lo que pensamos fue el concepto de "Red Amistosa" que seria una lista de usuarios que tiene a todos los usuarios que pueden relacionarse entre si con alguna cadena de amistades. Seria como separar el conjuntos de usuarios de una red en "Islas" y lo que hace la funcion redAmistosa seria mapear una de esas islas.
+    La funcion existeSecuenciaDeAmigos toma una RedSocial y dos usuarios. Devuelve True si existe una secuencia de relaciones entre usuarios que conecte al usuario 1
+    con el usuario 2 y devuelve False si no existe una secuencia de relaciones que los conecten o si estan directamente relacionados. 
+    Este problema era bastante complicado, por lo que encontramos un problema equivalente que nos solucionara la misma situacion.
+    Lo que pensamos fue el concepto de "Red Amistosa" que seria una lista de usuarios que tiene a todos los usuarios que pueden
+    relacionarse entre si con alguna cadena de amistades. Seria como separar el conjuntos de usuarios de una red en "Islas"
+    y lo que hace la funcion redAmistosa seria mapear una de esas islas.
     redAmistosa toma como parametros la Red Social a analizar y 2 listas. Una de usuarios visitados y otra de usuarios por visitar.
-    redAmistosa va iterando por todos los usuarios por visitar. En el paso recursivo se agrega el "usuario 1" a usuarios visitados y se arma la nueva lista de usuarios por explorar agregando UNICAMENTE los usuarios relacionados con el "usuario 1" que no hayan sido visitados anteriormente. Cuando no quedan usuarios por visitar, se devuelve la lista de visitados.
+    redAmistosa va iterando por todos los usuarios por visitar. En el paso recursivo se agrega el "usuario 1" a usuarios visitados
+    y se arma la nueva lista de usuarios por explorar agregando UNICAMENTE los usuarios relacionados con el "usuario 1" que no hayan sido visitados anteriormente.
+    Cuando no quedan usuarios por visitar, se devuelve la lista de visitados.
     Si "usuario 2" esta en esta lista, significa que puede conectarse con "usuario 1" de ALGUNA manera.
 -}
-
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
-existeSecuenciaDeAmigos r u1 u2 | pertenece (amigosDe r u1) u2 = False
-                                | otherwise = pertenece (redAmistosa r [u1] (amigosDe r u1)) u2
+existeSecuenciaDeAmigos red u1 u2 | pertenece u2 (amigosDe red u1) = False
+                                  | otherwise = pertenece u2 (redAmistosa red [u1] (amigosDe red u1))
 
 redAmistosa :: RedSocial -> [Usuario] -> [Usuario] -> [Usuario]
 redAmistosa red visitado [] = visitado
@@ -191,7 +196,7 @@ redAmistosa red visitado (u:us) = redAmistosa red (u:visitado) (porExplorar (ami
 
 porExplorar :: [Usuario] -> [Usuario] -> [Usuario] -> [Usuario]
 porExplorar [] us visitado = us
-porExplorar (x:xs) us visitado | pertenece visitado x || pertenece us x = porExplorar xs us visitado
+porExplorar (x:xs) us visitado | pertenece x visitado || pertenece x us = porExplorar xs us visitado
                                | otherwise = x : porExplorar xs us visitado
 
 {-union :: Eq a => [a] -> [a] -> [a]
@@ -203,6 +208,3 @@ quitar :: Eq a => [a] -> [a] -> [a]
 quitar [] y = []
 quitar (x:xs) y | pertenece y x = quitar xs y
                 | otherwise = x : quitar xs y-}
-
-
-
